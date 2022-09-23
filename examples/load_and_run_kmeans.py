e578fb6f60580cca7e27c98e86ffeb9a3afd4534
annabellegao <annabelle_gao@brown.edu>
2022-09-23 14:48:38 -0400

    added limits to y-axis

--- examples/load_and_run_kmeans.py
+++ examples/load_and_run_kmeans.py
@@ -2,9 +2,9 @@
 from nc_analysis import Dataset
 import matplotlib.pyplot as plt
 
-filename = "../nc_analysis/sample_data/DBRD-NATURE2020-depth.r0.1.nc"
+filename = "./nc_analysis/sample_data/DBRD-NATURE2020-depth.r0.1.nc"
 ds = Dataset(filename)
-model, x1, x2 = ds.kmeans('dvs', 'lQ', 4)
+x1, x2, model = ds.kmeans('dvs', 'lQ', 4)
 
 print("cluster centers:")
 print(model.cluster_centers_)
@@ -13,5 +13,6 @@ cluster_id = model.labels_
 plt.scatter(x1, x2, c=cluster_id)
 plt.xlabel('dvs')
 plt.ylabel('lQ')
+plt.ylim(0, 8)
 plt.title('cluster classification')
 plt.show()
